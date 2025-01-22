05ff6f0a61ef83630325b1c052c376a0b4dbc76c
Alunos <alunos@MacBook-Air-de-EDIT.local>
2025-01-22 22:34:18 +0000

    Exercicios aula 8 e 9

--- aula 9/script.js
+++ aula 9/script.js
@@ -1,5 +1,6 @@
+// PROMISES usando .then().catch()
 
-const soma = (num1,num2) => {  
+/* const soma = (num1,num2) => {  
 
 const result = num1 + num2;
 
@@ -14,4 +15,29 @@ const promise = new Promise((success,unsuccesful) =>{
  promise.then(resultado => console.log(resultado))
 .catch(resultado => console.log(resultado)); 
 }
+*/
 
+
+// PROMISES usando async/await
+const soma = async (num1,num2) => {  
+
+    const result = num1 + num2;
+    
+    const promise = new Promise((success,unsuccesful) =>{
+        if ( result % 2 === 0){
+            success('Numero par');
+        } else {
+             unsuccesful('Numero impar');
+            }
+
+
+    });   
+
+    try{
+        const resultPromise = await promise;
+        console.log(resultPromise) ;
+    } catch (e){
+        console.log(e);
+    }
+    
+    }
